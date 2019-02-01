9a5fd22cc78bedebd7efef490ca0ae63e3c5b5c1
Vilius Vystartas <vilius.vystartas@gmail.com>
2019-02-01 22:27:01 +0000

    Final commit, service is live

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# request-header-parser
+# Request Header Parser Microservice
+
+### GET request returns JSON object about the clients IP address, language preferences and software stack.
+
+### Live service can be accesssed here: https://vystartasv-request-header-parser.glitch.me/api/whoami
+
+### Example output bellow:
+
+```json
+{"ipaddress":"89.212.68.15","language":"en-GB,en;q=0.9,it-IT;q=0.8,it;q=0.7,lt-LT;q=0.6,lt;q=0.5,en-US;q=0.4","software":"Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.81 Safari/537.36"}
+```
--- server.js
+++ server.js
@@ -1,11 +1,20 @@
-var express = require('express');
+var express = require("express");
 
 var app = express(),
   port = process.env.PORT || 3000;
-  app.listen(port);
-  app.get('/', function(req, res) {
-    var answer = {"ipaddress":req.headers['host'], 
-    "language":req.headers['accept-language'],
-    "software":req.headers['user-agent']};
-    res.json(answer);
+app.listen(port);
+app.get("/", function(req, res) {
+  var ip = (
+    req.headers["x-forwarded-for"] ||
+    req.connection.remoteAddress ||
+    ""
+  )
+    .split(",")[0]
+    .trim();
+  var answer = {
+    ipaddress: ip,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  };
+  res.json(answer);
 });
